2f8decd151be5fc266d27dd4f28b385965d6c09b
Maria.Kotlyarevskaya <maria.kotlyarevskaya@nordigy.ru>
2019-09-05 23:18:58 +0300

    Submit triangle task

--- triangle/triangle.go
+++ triangle/triangle.go
@@ -5,16 +5,19 @@
 // https://golang.org/doc/effective_go.html#commentary
 package triangle
 
+import (
+	"math"
+)
 
 // Notice KindFromSides() returns this type. Pick a suitable data type.
-type Kind
+type Kind string
 
 const (
-    // Pick values for the following identifiers used by the test program.
-    NaT // not a triangle
-    Equ // equilateral
-    Iso // isosceles
-    Sca // scalene
+	// Pick values for the following identifiers used by the test program.
+	NaT = Kind("NaT") // not a triangle
+	Equ = Kind("Equ") // equilateral
+	Iso = Kind("Iso") // isosceles
+	Sca = Kind("Sca") // scalene
 )
 
 // KindFromSides should have a comment documenting it.
@@ -23,6 +26,15 @@ func KindFromSides(a, b, c float64) Kind {
 	// Then remove all the stock comments.
 	// They're here to help you get started but they only clutter a finished solution.
 	// If you leave them in, reviewers may protest!
-	var k Kind
-	return k
+	if a <= 0 || b <= 0 || c <= 0 || a+b < c || a+c < b || b+c < a || math.IsNaN(a+b+c) || math.IsInf(a+b+c, 0) {
+		return NaT
+	}
+	if a == b && a == c {
+		return Equ
+	} else if a == b || a == c || b == c {
+		return Iso
+	} else if a+b >= c || b+c >= a || a+c >= b {
+		return Sca
+	}
+	return NaT
 }
