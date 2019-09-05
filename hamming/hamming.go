c52044e0a42343965bd9a6031cfa8f2294645771
Maria.Kotlyarevskaya <maria.kotlyarevskaya@nordigy.ru>
2019-09-05 22:27:52 +0300

    Use rune type instead of bytes comparing

--- hamming/hamming.go
+++ hamming/hamming.go
@@ -3,23 +3,21 @@ package hamming
 
 import (
 	"errors"
-
-	"github.com/mozillazg/go-unidecode"
 )
 
 // Distance is func which returns hamming difference between two DNAs
 func Distance(a, b string) (int, error) {
 	count := 0
 	var err error
-	a = unidecode.Unidecode(a)
-	b = unidecode.Unidecode(b)
-	if len(a) == len(b) && (len(a) != 0 || len(b) != 0) {
-		for i := 0; i < len(a); i++ {
-			if a[i] != b[i] {
+	aRune := []rune(a)
+	bRune := []rune(b)
+	if len(aRune) == len(bRune) && (len(aRune) != 0 || len(bRune) != 0) {
+		for i := 0; i < len(aRune); i++ {
+			if aRune[i] != bRune[i] {
 				count++
 			}
 		}
-	} else if len(a) != len(b) {
+	} else if len(aRune) != len(bRune) {
 		err = errors.New("DNAs should have the same length")
 	}
 	return count, err
