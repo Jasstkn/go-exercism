1cd2161e84908cb5ba94b20a8f93c3487156fa5f
Jasstkn <mariia.kotliarevskaia@gmail.com>
2022-07-02 20:10:51 +0200

    better: pythagorean_triplet
    Signed-off-by: Jasstkn <mariia.kotliarevskaia@gmail.com>

--- pythagorean-triplet/pythagorean_triplet.go
+++ pythagorean-triplet/pythagorean_triplet.go
@@ -1,16 +1,18 @@
 package pythagorean
 
+import "math"
+
 type Triplet [3]int
 
 // Range generates list of all Pythagorean triplets with side lengths
 // in the provided range.
 func Range(min, max int) (triplets []Triplet) {
-	for v1 := min; v1 <= max; v1++ {
-		for v2 := min; v2 <= max; v2++ {
-			for v3 := min; v3 <= max; v3++ {
-				if v1*v1+v2*v2 == v3*v3 && v1 < v2 && v2 < v3 {
-					triplets = append(triplets, Triplet{v1, v2, v3})
-				}
+	for a := min; a <= max; a++ {
+		for b := min; b <= max; b++ {
+			c := math.Sqrt(math.Pow(float64(a), 2) + math.Pow(float64(b), 2))
+			cInt := math.Trunc(c)
+			if cInt == c && a < b && float64(b) < c && c <= float64(max) {
+				triplets = append(triplets, Triplet{a, b, int(c)})
 			}
 		}
 	}
@@ -20,7 +22,7 @@ func Range(min, max int) (triplets []Triplet) {
 // Sum returns a list of all Pythagorean triplets with a certain perimeter.
 func Sum(p int) (triplets []Triplet) {
 	for _, t := range Range(0, p) {
-		if t[0]+t[1]+t[2] == p{
+		if t[0]+t[1]+t[2] == p {
 			triplets = append(triplets, t)
 		}
 	}
