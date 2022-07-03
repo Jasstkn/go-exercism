3cedcf36aafeb7ada96a0491fcdef579cc5d9ab0
Jasstkn <mariia.kotliarevskaia@gmail.com>
2022-07-03 14:59:13 +0200

    better: pythagorean_triplet
    Signed-off-by: Jasstkn <mariia.kotliarevskaia@gmail.com>

--- pythagorean-triplet/pythagorean_triplet.go
+++ pythagorean-triplet/pythagorean_triplet.go
@@ -8,10 +8,9 @@ type Triplet [3]int
 // in the provided range.
 func Range(min, max int) (triplets []Triplet) {
 	for a := min; a <= max; a++ {
-		for b := min; b <= max; b++ {
-			c := math.Sqrt(math.Pow(float64(a), 2) + math.Pow(float64(b), 2))
-			cInt := math.Trunc(c)
-			if cInt == c && a < b && float64(b) < c && c <= float64(max) {
+		for b := a + 1; b <= max; b++ {
+			c := math.Sqrt(float64(a*a + b*b))
+			if c == float64(int(c)) && int(c) <= max {
 				triplets = append(triplets, Triplet{a, b, int(c)})
 			}
 		}
@@ -21,7 +20,7 @@ func Range(min, max int) (triplets []Triplet) {
 
 // Sum returns a list of all Pythagorean triplets with a certain perimeter.
 func Sum(p int) (triplets []Triplet) {
-	for _, t := range Range(0, p) {
+	for _, t := range Range(1, p/2) {
 		if t[0]+t[1]+t[2] == p {
 			triplets = append(triplets, t)
 		}
