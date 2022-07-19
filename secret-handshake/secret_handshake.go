e456b1d0836653dad8aac6052543ce7f23b507ec
Jasstkn <mariia.kotliarevskaia@gmail.com>
2022-07-19 08:48:31 +0200

    better: secret_handshake
    Signed-off-by: Jasstkn <mariia.kotliarevskaia@gmail.com>

--- secret-handshake/secret_handshake.go
+++ secret-handshake/secret_handshake.go
@@ -1,35 +1,29 @@
 package secret
 
-var codeMapping = map[int]string{
-	1:    "wink",
-	10:   "double blink",
-	100:  "close your eyes",
-	1000: "jump",
-}
-
 func reverse(input []string) []string {
-	i := 0
-	j := len(input) - 1
-	for i < j {
+	for i, j := 0, len(input) - 1; i < j; i, j = i+1, j-1 {
 		input[i], input[j] = input[j], input[i]
-		i++
-		j--
 	}
 	return input
 }
 
 func Handshake(code uint) (out []string) {
+	// 1 is wink
+	// 10 is double blink
+	// 100 is close your eyes
+	// 1000 is jump
+	// 10000 is to revert array
 	if code&1 > 0 {
-		out = append(out, codeMapping[1])
+		out = append(out, "wink")
 	}
 	if code&2 > 0 {
-		out = append(out, codeMapping[10])
+		out = append(out, "double blink")
 	}
 	if code&4 > 0 {
-		out = append(out, codeMapping[100])
+		out = append(out, "close your eyes")
 	}
 	if code&8 > 0 {
-		out = append(out, codeMapping[1000])
+		out = append(out, "jump")
 	}
 
 	if code&16 > 0 {
